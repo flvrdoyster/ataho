714f38a89cb7cb92af069757c18b4ac1c0274441
flvrdoyster <55177008+flvrdoyster@users.noreply.github.com>
2023-12-01 22:33:21 +0900

    버튼을 누르면 이미지를 변경

---
+++ swim.js
@@ -0,0 +1,8 @@
+// swim.js
+
+function toggleImg1() {
+    document.getElementById("ataho").src = "ataho_swim.gif";
+}
+function toggleImg2() {
+    document.getElementById("ataho").src = "ataho_idle.gif";
+}
