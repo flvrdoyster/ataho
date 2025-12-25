f41d9ca5b43f7d418f757ba428f1519d092ad877
flvrdoyster <flvrdoyster@naver.com>
2025-12-25 09:16:02 +0900

    쾌진극 작업, 영상 뷰어 추가

--- world/maps/cave/triggers.js
+++ world/maps/cave/triggers.js
@@ -7,5 +7,6 @@ window.MAP_DATA.triggers = window.MAP_DATA.triggers.concat([
     { "x": 14, "y": 16, "w": 4, "h": 4, "targetId": "popup-haiyuki" },
     { "x": 15, "y": 15, "w": 6, "h": 2, "targetId": "popup-haiyuki" },
     { "x": 19, "y": 16, "w": 4, "h": 4, "targetId": "popup-haiyuki" },
-    { "x": 5, "y": 17, "w": 2, "h": 2, "targetId": "popup-gallery" }
+    { "x": 5, "y": 17, "w": 2, "h": 2, "targetId": "popup-gallery" },
+    { "x": 32, "y": 20, "w": 2, "h": 2, "targetId": "popup-ani" },
 ]);
